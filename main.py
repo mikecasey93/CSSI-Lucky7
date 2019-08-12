854da4d903ba6d665564190cab79f90b840b52ff
mikecasey93 <caseymichaelt1893@gamil.com>
2019-08-12 15:25:35 -0400

    First Commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import webapp2
+import os
+import random
+import jinja2
+import datetime
+
+jinja_current_dir = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class DisplayHandler(webapp2.RequestHandler):
+    def get(self):
+        start_template = jinja_current_dir.get_template("templates/WelcomePage.html")
+        self.response.write(start_template.render())
+
+    #Verifies the user's age
+    def post(self):
+        user_age = self.request.get('Users-age')
+
+        if user_age < 18:
+            self.response.write("Sorry you cannot use our site")
+        else:
+            self.response.write("Welcome to our site we hope you enjoy")
+
+class NumberInputHandler(webapp2.RequestHandler):
+    def get(self):
+        start_template = jinja_current_dir.get_template("templates/select.html")
+        self.response.write(start_template.render())
+
+    def post(self):
+
+        n1 = self.request.get('n1')
+        n2 = self.request.get('n2')
+        n3 = self.request.get('n3')
+        n4 = self.request.get('n4')
+        n5 = self.request.get('n5')
+        n6 = self.request.get('n6')
+        n7 = self.request.get('n7')
+        self.response.write(n1 + n2 + n3 + n4 + n5 + n6 + n7)
+
+app = webapp2.WSGIApplication([
+('/', DisplayHandler),
+('/numberInput', NumberInputHandler)
+], debug=True)
